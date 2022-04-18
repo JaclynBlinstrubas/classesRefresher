324e5e1da9ef88e4328b368a1795de5833b93852
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2022-04-18 14:12:41 -0400

    newAnimal

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function Animal(name) {
-  this.name = name
-}
+// Prior to ES6, to create a class - use prototypes similar to this:
 
-Animal.prototype.printName = function () {
-  console.log(this.name)
+// function Animal(name) {
+//   this.name = name
+// }
+
+// Animal.prototype.printName = function () {
+//   console.log(this.name)
+// }
+
+// let duck = new Animal('duck')
+// duck.printName()
+
+// ES6 class keyword
+
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+
+  printName() {
+    console.log(this.name)
+  }
 }
 
 let duck = new Animal('duck')
 duck.printName()
+
+console.log(typeof Animal)
