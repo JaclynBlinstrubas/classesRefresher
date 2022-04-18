20f665aa6bfcf3ac3977235c656ad887d5e05304
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2022-04-18 14:26:01 -0400

    'extends' keyword

--- script.js
+++ script.js
@@ -11,19 +11,49 @@
 // let duck = new Animal('duck')
 // duck.printName()
 
-// ES6 class keyword
+// ------------------------
 
-class Animal {
+// ES6 'CLASS' KEYWORD
+
+// class Animal {
+//   constructor(name) {
+//     this.name = name
+//   }
+
+//   printName() {
+//     console.log(this.name)
+//   }
+// }
+
+// let duck = new Animal('duck')
+// duck.printName()
+
+// console.log(typeof Animal)
+
+// ------------------------
+
+// 'EXTENDS' KEYWORD
+
+class Parent {
   constructor(name) {
     this.name = name
   }
 
-  printName() {
-    console.log(this.name)
+  getName() {
+    return this.name
+  }
+}
+// assume you needed to create a subclass named Child that extends the functionality available in the Parent class:
+
+class Child extends Parent {
+  constructor(name) {
+    super(name)
+  }
+
+  getMessage() {
+    return 'Hello ' + super.getName()
   }
 }
 
-let duck = new Animal('duck')
-duck.printName()
-
-console.log(typeof Animal)
+let someone = new Child('person')
+console.log(someone.getMessage())
